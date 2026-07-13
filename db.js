9b9eddf6e9f606aeae8a063a2f8f2ac4b958d407
Dassaevy Ono <dassajono@gmail.com>
2026-07-12 22:20:35 -0300

    Configurando conexão com banco usando variáveis de ambiente

--- db.js
+++ db.js
@@ -3,25 +3,22 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
-
 });
 
 db.getConnection((err, connection) => {
 
     if (err) {
-
         console.error("❌ Erro ao conectar ao MySQL:", err);
         return;
-
     }
 
     console.log("✅ Conectado ao MySQL!");
